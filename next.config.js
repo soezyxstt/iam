d0c79a0791bd73ea35f76a197e3a316876d29248
Adi Haditya Nursyam <13122080@mahasiswa.itb.ac.id>
2026-08-03 15:04:55 +0700

    ci: setup github actions deploy workflow for jagoan hosting cloud mixmatch

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const NEXT_PUBLIC_SERVER_URL =
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
